6ab99751e82b0b66a6f4fea8ebf049ddbaeaa679
Thiago Ramos <thiagorbernardo@hotmail.com>
2021-05-20 20:20:19 -0300

    refactor: use packages and events

---
+++ src/main/java/com/mycompany/app/Projectiles/Bullet.java
@@ -0,0 +1,49 @@
+package com.mycompany.app.Projectiles;
+
+import com.almasb.fxgl.entity.component.Component;
+import com.almasb.fxgl.texture.Texture;
+import com.mycompany.app.Characters.PlayerTypes;
+import javafx.geometry.Point2D;
+
+import static com.almasb.fxgl.dsl.FXGL.image;
+
+public class Bullet extends Component {
+    private double speed;
+    private double xDirection;
+    private double yDirection;
+    private PlayerTypes bulletOwner;
+    private Texture texture;
+
+    public Bullet(PlayerTypes bulletOwner, String path, Point2D destiny, double speed) {
+        this.bulletOwner = bulletOwner;
+        this.speed = speed;
+        this.xDirection = destiny.getX();
+        this.yDirection = destiny.getY();
+        this.texture = new Texture(image("projectiles/" + path + ".png"));
+    }
+
+    @Override
+    public void onAdded() {
+        this.entity.getViewComponent().addChild(this.texture);
+    }
+
+    @Override
+    public void onUpdate(double tpf) {
+        if(this.xDirection > 0)
+            entity.translateX(tpf * this.speed);
+        else if(this.xDirection < 0)
+            entity.translateX(-tpf * this.speed);
+        if(this.yDirection > 0)
+            entity.translateY(tpf * this.speed);
+        else if(this.yDirection < 0)
+            entity.translateY(-tpf * this.speed);
+    }
+
+    public void increaseSpeed(){
+        this.speed += 100;
+    }
+
+    public PlayerTypes getBulletOwner() {
+        return this.bulletOwner;
+    }
+}

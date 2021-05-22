47ebe747672448a156d9d1d17ccb0d2e00e819e4
Thiago Ramos <thiagorbernardo@hotmail.com>
2021-05-22 01:52:09 -0300

    feat: finish save dao

--- src/main/java/com/mycompany/app/Save/Save.java
+++ src/main/java/com/mycompany/app/Save/Save.java
@@ -16,7 +16,8 @@ public class Save {
     public Save(boolean isMultiplayer, int level, Map<PlayerTypes, Entity> players) {
         this.isMultiplayer = isMultiplayer;
         this.level = level;
-        this.mapsToObjects(players);
+        if(players != null)
+            this.mapsToObjects(players);
     }
 
     private void mapsToObjects(Map<PlayerTypes, Entity> players){
@@ -27,4 +28,23 @@ public class Save {
             this.P2Data = players.get(PlayerTypes.P2).getComponent(Player.class).getPlayerData();
     }
 
+    public void setCreatedAt(long createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public void setMultiplayer(boolean multiplayer) {
+        isMultiplayer = multiplayer;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public void setP1Data(Data p1Data) {
+        P1Data = p1Data;
+    }
+
+    public void setP2Data(Data p2Data) {
+        P2Data = p2Data;
+    }
 }
--- src/main/java/com/mycompany/app/Save/SaveTXT.java
+++ src/main/java/com/mycompany/app/Save/SaveTXT.java
@@ -1,13 +1,118 @@
 package com.mycompany.app.Save;
 
-public class SaveTXT implements SaveDAO{
+import com.mycompany.app.Power.PowerType;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+import java.nio.charset.StandardCharsets;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class SaveTXT implements SaveDAO {
+    private String filePath = "src/main/resources/save/save.txt";
+
+    private FileManager fileManager = new FileManager("src/main/resources/save/save.txt", "");
+
+    private Map<String, String> saveMap = new HashMap<>();
+
     @Override
     public Save read() {
-        return null;
+        this.fileManager.createFile();
+
+        try {
+            File file = new File(this.filePath);
+
+            FileReader fileReader = new FileReader(file, StandardCharsets.UTF_8);
+
+            BufferedReader br = new BufferedReader(fileReader);
+
+            Pattern saveRegex = Pattern.compile("(.+):(.+)");
+
+            String line;
+            while ((line = br.readLine()) != null) {
+                Matcher matcher = saveRegex.matcher(line);
+                if (matcher.find()) {
+                    saveMap.put(matcher.group(1), matcher.group(2));
+                }
+            }
+            fileReader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return this.mapToSave();
     }
 
     @Override
     public void save(Save save) {
+        StringBuilder savingString = new StringBuilder();
 
+        /* Saving world data*/
+        savingString.append("createdAt").append(":").append(save.createdAt).append("\n");
+        savingString.append("isMultiplayer").append(":").append(save.isMultiplayer).append("\n");
+        savingString.append("level").append(":").append(save.level).append("\n");
+
+        savingString.append("P1Data.points").append(":").append(save.P1Data.getPoints()).append("\n");
+        savingString.append("P1Data.speed").append(":").append(save.P1Data.getSpeed()).append("\n");
+        if(save.P1Data.getActivePower() != null)
+            savingString.append("P1Data.activePower").append(":").append(save.P1Data.getActivePower()).append("\n");
+        savingString.append("P1Data.lastShot").append(":").append(save.P1Data.getLastShot()).append("\n");
+        savingString.append("P1Data.life").append(":").append(save.P1Data.getLife()).append("\n");
+        savingString.append("P1Data.fireRate").append(":").append(save.P1Data.getFireRate()).append("\n");
+        savingString.append("P1Data.isInvincible").append(":").append(save.P1Data.getInvincibility()).append("\n");
+
+        if(save.P2Data != null){
+            savingString.append("P2Data.points").append(":").append(save.P2Data.getPoints()).append("\n");
+            savingString.append("P2Data.speed").append(":").append(save.P2Data.getSpeed()).append("\n");
+            if(save.P2Data.getActivePower() != null)
+                savingString.append("P2Data.activePower").append(":").append(save.P2Data.getActivePower()).append("\n");
+            savingString.append("P2Data.lastShot").append(":").append(save.P2Data.getLastShot()).append("\n");
+            savingString.append("P2Data.life").append(":").append(save.P2Data.getLife()).append("\n");
+            savingString.append("P2Data.fireRate").append(":").append(save.P2Data.getFireRate()).append("\n");
+            savingString.append("P2Data.isInvincible").append(":").append(save.P2Data.getInvincibility()).append("\n");
+        }
+
+        this.fileManager.saveString(savingString.toString(), false);
+    }
+
+    private Save mapToSave() {
+        Save save = new Save(false, 1, null);
+
+        save.setCreatedAt(Long.parseLong(this.saveMap.get("createdAt")));
+        save.setMultiplayer(Boolean.parseBoolean(this.saveMap.get("isMultiplayer")));
+        save.setLevel(Integer.parseInt(this.saveMap.get("level")));
+
+        Data p1Data = new Data(
+                Double.parseDouble(this.saveMap.get("P1Data.points")),
+                Integer.parseInt(this.saveMap.get("P1Data.speed")),
+                this.saveMap.get("P1Data.activePower") != null ? PowerType.valueOf(this.saveMap.get("P1Data.activePower")) : null,
+                Double.parseDouble(this.saveMap.get("P1Data.lastShot")),
+                Integer.parseInt(this.saveMap.get("P1Data.life")),
+                Double.parseDouble(this.saveMap.get("P1Data.fireRate")),
+                Boolean.parseBoolean(this.saveMap.get("P1Data.isInvincible"))
+        );
+
+        save.setP1Data(p1Data);
+
+        if (save.isMultiplayer) {
+            Data p2Data = new Data(
+                    Double.parseDouble(this.saveMap.get("P2Data.points")),
+                    Integer.parseInt(this.saveMap.get("P2Data.speed")),
+                    this.saveMap.get("P2Data.activePower") != null ? PowerType.valueOf(this.saveMap.get("P2Data.activePower")) : null,
+                    Double.parseDouble(this.saveMap.get("P2Data.lastShot")),
+                    Integer.parseInt(this.saveMap.get("P2Data.life")),
+                    Double.parseDouble(this.saveMap.get("P2Data.fireRate")),
+                    Boolean.parseBoolean(this.saveMap.get("P2Data.isInvincible"))
+            );
+
+            save.setP2Data(p2Data);
+        }
+
+        return save;
     }
 }
